da7391a118a6debbd7150fd46452465587011449
gaoliang <gaoliangim@gmail.com>
2020-12-10 01:14:52 +0800

    初始化项目结构

---
+++ vue.config.js
@@ -0,0 +1,28 @@
+const path = require('path');
+
+module.exports = {
+    pages: {
+        index: {
+            // 页面入口
+            entry: "examples/main.js",
+            // 模板来源
+            template: "pubilc/index.html",
+            // 输出文件名
+            filename: "index.html"
+        }
+    },
+    configureWebpack: config => {
+        if (process.env.NODE_ENV === 'production') {
+            // 为生产环境修改配置...
+        } else {
+            // 为开发环境修改配置...
+        }
+        return {
+            resolve: {
+                alias: {
+                    "@": require("path").resolve(__dirname, "examples")
+                }
+            }
+        }
+    }
+};
